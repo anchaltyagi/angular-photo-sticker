f5238bf406e37314bc2edfab9fd2f562bfa6944e
anciityagi <atyagi@mirthcorp.com>
2015-10-18 00:26:34 +0530

    Implemented feature to drop same sticker mutiple times and sticker modal validation

--- controllers/stickerController.js
+++ controllers/stickerController.js
@@ -2,6 +2,9 @@ angular
     .module('blipparPhotoStickerApp')
     .controller('stickerController', function($scope, fileReader){
         $scope.stickers =[];
+        $scope.newSticker ={};
+        $scope.stickerCounter = 0;
+
     $scope.getFile = function (callback) {
         $scope.progress = 0;
         fileReader.readAsDataUrl($scope.file, $scope)
@@ -15,13 +18,28 @@ angular
     });
 
     $scope.dropCallback = function(event, ui) {
-        
+        var droppedImage = ui.draggable[0];
+        var droppedImageIndex = $scope.stickers.findIndex(function(element, index){
+             return element.id === droppedImage.id ? index : -1;
+        });
+        //Recreate dropped image for providing the option to drop mutiple times
+        if(droppedImageIndex > -1){
+            //Reset the height of dropped image parent container
+            droppedImage.parentElement.style.height = "0px";
+            
+            var recreateSticker = {
+                            id: ++$scope.stickerCounter,
+                            image: droppedImage.currentSrc
+                        };
+            $scope.stickers.push(recreateSticker);
+        }
     };
 
-    $scope.dragCallback = function(event, ui){
-        var draggingImage = event.target.currentSrc;
-        $scope.stickers.push(draggingImage);
-
+    $scope.uploadSticker = function(){
+        $scope.stickers.push(angular.copy($scope.newSticker));
+        //Reset sticker upload modal window
+        $scope.newSticker ={};
+        $scope.stickerImageSrc = "";
     }
  	
 });
--- directives/uploadPhotoDragPicture.js
+++ directives/uploadPhotoDragPicture.js
@@ -3,28 +3,31 @@
 /* Directives */
 angular
     .module('blipparPhotoStickerApp')
-     .directive('uploadImage', function() {
+    .directive('uploadImage', function() {
         return {
             link: function($scope, element, attrs) {
-                element.bind("change", function(e){      
+                element.bind("change", function(e) {
                     $scope.file = (e.srcElement || e.target).files[0];
-                    $scope.getFile(function(err, result){
+                    $scope.getFile(function(err, result) {
                         $scope.imageSrc = result;
                     });
-                  });
+                });
             }
         };
     })
     .directive('uploadSticker', function() {
         return {
             link: function($scope, element, attrs) {
-                element.bind("change", function(e){      
+                element.bind("change", function(e) {
                     $scope.file = (e.srcElement || e.target).files[0];
-                    $scope.getFile(function(err, result){
-                        $scope.stickers.push({image:result});
+                    $scope.getFile(function(err, result) {
+                        $scope.newSticker = {
+                            id: ++$scope.stickerCounter,
+                            image: result
+                        };
                         $scope.stickerImageSrc = result;
                     });
                 });
             }
         };
-});
+    });
