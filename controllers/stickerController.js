d9d95577713dd857f99b8b4d04a01553be0ba640
anciityagi <atyagi@mirthcorp.com>
2015-10-18 12:50:39 +0530

    Implemented image title and maintain title after dropping image

--- controllers/stickerController.js
+++ controllers/stickerController.js
@@ -1,45 +1,47 @@
 angular
     .module('blipparPhotoStickerApp')
-    .controller('stickerController', function($scope, fileReader){
-        $scope.stickers =[];
-        $scope.newSticker ={};
+    .controller('stickerController', function($scope, fileReader) {
+        $scope.stickers = [];
+        $scope.newSticker = {};
         $scope.stickerCounter = 0;
 
-    $scope.getFile = function (callback) {
-        $scope.progress = 0;
-        fileReader.readAsDataUrl($scope.file, $scope)
-                      .then(function(result) {
-                          callback(null,result);
-                      });
-    };
- 
-    $scope.$on("fileProgress", function(e, progress) {
-        $scope.progress = progress.loaded / progress.total;
-    });
+        $scope.getFile = function(callback) {
+            $scope.progress = 0;
+            fileReader.readAsDataUrl($scope.file, $scope)
+                .then(function(result) {
+                    callback(null, result);
+                });
+        };
 
-    $scope.dropCallback = function(event, ui) {
-        var droppedImage = ui.draggable[0];
-        var droppedImageIndex = $scope.stickers.findIndex(function(element, index){
-             return element.id === droppedImage.id ? index : -1;
+        $scope.$on("fileProgress", function(e, progress) {
+            $scope.progress = progress.loaded / progress.total;
         });
-        //Recreate dropped image for providing the option to drop mutiple times
-        if(droppedImageIndex > -1){
+
+        $scope.dropCallback = function(event, ui) {
+            var droppedImage = ui.draggable[0];
             //Reset the height of dropped image parent container
             droppedImage.parentElement.style.height = "0px";
-            
-            var recreateSticker = {
-                            id: ++$scope.stickerCounter,
-                            image: droppedImage.currentSrc
-                        };
-            $scope.stickers.push(recreateSticker);
-        }
-    };
 
-    $scope.uploadSticker = function(){
-        $scope.stickers.push(angular.copy($scope.newSticker));
-        //Reset sticker upload modal window
-        $scope.newSticker ={};
-        $scope.stickerImageSrc = "";
-    }
- 	
-});
+            //Remove span for image title
+            var titleSpan = droppedImage.parentElement.getElementsByTagName("span");
+            if (titleSpan && titleSpan[0]) {
+                droppedImage.parentElement.removeChild(titleSpan[0]);
+            }
+
+            //Recreate image after drop for using mutiple times
+            var recreateSticker = {
+                id: ++$scope.stickerCounter,
+                image: droppedImage.currentSrc,
+                title: droppedImage.title
+            };
+            $scope.stickers.push(recreateSticker);
+        };
+
+        $scope.uploadSticker = function() {
+            $scope.stickers.push(angular.copy($scope.newSticker));
+            //Reset sticker upload modal window
+            $scope.newSticker = {};
+            $scope.stickerImageSrc = "";
+        }
+
+    });
