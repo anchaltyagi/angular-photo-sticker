2a920ec0eb194fb63ac0414693021e0877e4c6e1
anciityagi <atyagi@mirthcorp.com>
2015-12-01 14:48:21 +0530

    controller as with directive

--- controllers/stickerController.js
+++ controllers/stickerController.js
@@ -1,96 +1,93 @@
-(function(module) {
+ var stickerController = function($scope, fileReader, localStorageService) {
+     var stickersInStore = localStorageService.get('stickers');
+     var mainPhoto = localStorageService.get('mainPhoto');
 
-    var stickerController = function($scope, fileReader, localStorageService) {
-        var stickersInStore = localStorageService.get('stickers');
-        var mainPhoto = localStorageService.get('mainPhoto');
+     var vm = this;
 
-        var vm = this;
-
-        vm.stickers = stickersInStore || []; // Load stickers from local storage
-        vm.imageSrc = mainPhoto || ""; // Load main photo image from local storage
-        vm.newSticker = {};
+     vm.stickers = stickersInStore || []; // Load stickers from local storage
+     vm.imageSrc = mainPhoto || ""; // Load main photo image from local storage
+     vm.newSticker = {};
 
 
 
-        //*********************************************
-        // WATCHES
-        //*********************************************
+     //*********************************************
+     // WATCHES
+     //*********************************************
 
-        //Save sticker in local store
-        $scope.$watch('vm.stickers', function() {
-            try {
-                localStorageService.set('stickers', $scope.stickers);
-            } catch (e) {
-                alert("Local storage size exceeded from 5MB.");
-            }
-        }, true);
+     //Save sticker in local store
+     $scope.$watch('vm.stickers', function() {
+         try {
+             localStorageService.set('stickers', $scope.stickers);
+         } catch (e) {
+             alert("Local storage size exceeded from 5MB.");
+         }
+     }, true);
 
-        //Save main photo in local store
-        $scope.$watch('vm.imageSrc', function() {
-            try {
-                localStorageService.set('mainPhoto', vm.imageSrc);
-            } catch (e) {
-                alert("Local storage size exceeded from 5MB.");
-            }
-        }, true);
+     //Save main photo in local store
+     $scope.$watch('vm.imageSrc', function() {
+         try {
+             localStorageService.set('mainPhoto', vm.imageSrc);
+         } catch (e) {
+             alert("Local storage size exceeded from 5MB.");
+         }
+     }, true);
 
-        $scope.getFile = function(callback) {
-            $scope.progress = 0;
-            fileReader.readAsDataUrl($scope.file, $scope)
-                .then(function(result) {
-                    callback(null, result, vm);
-                });
-        };
+     var getFile = function(callback) {
+         $scope.progress = 0;
+         fileReader.readAsDataUrl($scope.vm.file, $scope)
+             .then(function(result) {
+                 callback(null, result);
+             });
+     };
 
-        //*********************************************
-        // Local Functions
-        //*********************************************
-        var dropCallback = function(event, ui) {
-            var droppedImageBlock = ui.draggable[0];
+     //*********************************************
+     // Local Functions
+     //*********************************************
+     var dropCallback = function(event, ui) {
+         var droppedImageBlock = ui.draggable[0];
 
-            //Reset the height of dropped image parent container
-            droppedImageBlock.parentElement.style.height = "0px";
+         //Reset the height of dropped image parent container
+         droppedImageBlock.parentElement.style.height = "0px";
 
-            //Recreate image after drop for using mutiple times, don't recreate if image is repositioned after drop within dropped area
-            if (!droppedImageBlock.title.startsWith("dropped_")) {
-                var droppedImage = droppedImageBlock.getElementsByTagName("img");
-                if (droppedImage && droppedImage[0]) {
-                    droppedImage = droppedImage[0];
-                }
-                var recreateSticker = {
-                    image: droppedImage.currentSrc,
-                    title: droppedImageBlock.title
-                };
-                vm.stickers.push(angular.copy(recreateSticker));
-                droppedImageBlock.title = "dropped_" + droppedImageBlock.id;
-            }
-        };
+         //Recreate image after drop for using mutiple times, don't recreate if image is repositioned after drop within dropped area
+         if (!droppedImageBlock.title.startsWith("dropped_")) {
+             var droppedImage = droppedImageBlock.getElementsByTagName("img");
+             if (droppedImage && droppedImage[0]) {
+                 droppedImage = droppedImage[0];
+             }
+             var recreateSticker = {
+                 image: droppedImage.currentSrc,
+                 title: droppedImageBlock.title
+             };
+             vm.stickers.push(angular.copy(recreateSticker));
+             droppedImageBlock.title = "dropped_" + droppedImageBlock.id;
+         }
+     };
 
-        var uploadSticker = function() {
-            var newSticker = angular.copy(vm.newSticker);
-            vm.stickers.push(newSticker);
+     var uploadSticker = function() {
+         var newSticker = angular.copy(vm.newSticker);
+         vm.stickers.push(newSticker);
 
-            //Reset sticker upload modal window
-            vm.newSticker = {};
-            vm.stickerImageSrc = "";
-        }
+         //Reset sticker upload modal window
+         vm.newSticker = {};
+         vm.stickerImageSrc = "";
+     }
 
-        var reset = function() {
-            vm.stickers = [];
-            vm.imageSrc = "";
-        }
+     var reset = function() {
+         vm.stickers = [];
+         vm.imageSrc = "";
+     }
 
-        var removeSticker = function(index) {
-            vm.stickers.splice(index, 1);
-        }
+     var removeSticker = function(index) {
+         vm.stickers.splice(index, 1);
+     }
 
-        vm.dropCallback = dropCallback;
-        vm.uploadSticker = uploadSticker;
-        vm.reset = reset;
-        vm.removeSticker = removeSticker;
-    }
+     vm.dropCallback = dropCallback;
+     vm.uploadSticker = uploadSticker;
+     vm.reset = reset;
+     vm.removeSticker = removeSticker;
+     vm.getFile = getFile;
+ }
 
-    stickerController['$inject'] = ['$scope', 'fileReader', 'localStorageService'];
-    module.controller('stickerController', stickerController);
-
-}(angular.module("photoStickerApp")));
+ stickerController['$inject'] = ['$scope', 'fileReader', 'localStorageService'];
+ angular.module("photoStickerApp").controller('stickerController', stickerController);
--- directives/uploadPhotoDragPicture.js
+++ directives/uploadPhotoDragPicture.js
@@ -1,34 +1,56 @@
-'use strict';
-
-/* Directives */
+//*********************************************
+// Directive to upload base image
+//*********************************************
 angular
     .module('photoStickerApp')
-    .directive('uploadImage', function() {
-        return {
-            link: function($scope, element, attrs) {
-                //Read selected image and show in photo area
-                element.bind("change", function(e) {
-                    $scope.file = (e.srcElement || e.target).files[0];
-                    $scope.getFile(function(err, result, vm) {
-                        vm.imageSrc = result;
-                    });
-                });
-            }
-        };
-    })
-    .directive('uploadSticker', function() {
-        return {
-            link: function($scope, element, attrs) {
-                //Read selected image and create new model scope for sticker modal window
-                element.bind("change", function(e) {
-                    $scope.file = (e.srcElement || e.target).files[0];
-                    $scope.getFile(function(err, result, vm) {
-                        vm.newSticker = {
-                            image: result
-                        };
-                        vm.stickerImageSrc = result;
-                    });
-                });
-            }
-        };
-    });
+    .directive('uploadImage', uploadImage);
+
+function uploadImage() {
+    var directive = {
+        link: linkFunc,
+        controller: stickerController,
+        controllerAs: 'vm',
+        bindToController: true
+    }
+    return directive;
+
+    function linkFunc($scope, element, attrs) {
+        //Read selected image and show in photo area
+        element.bind("change", function(e) {
+            $scope.vm.file = (e.srcElement || e.target).files[0];
+            $scope.vm.getFile(function(err, result) {
+                $scope.vm.imageSrc = result;
+            });
+        });
+    }
+}
+
+//*********************************************
+// Directive to upload sticker
+//*********************************************
+angular
+    .module('photoStickerApp')
+    .directive('uploadSticker', uploadSticker);
+
+function uploadSticker() {
+    var directive = {
+        link: linkFunc,
+        controller: stickerController,
+        controllerAs: 'vm',
+        bindToController: true
+    }
+    return directive;
+
+    function linkFunc($scope, element, attrs) {
+        //Read selected image and show in photo area
+        element.bind("change", function(e) {
+            $scope.vm.file = (e.srcElement || e.target).files[0];
+            $scope.vm.getFile(function(err, result) {
+                $scope.vm.newSticker = {
+                    image: result
+                };
+                $scope.vm.stickerImageSrc = result;
+            });
+        });
+    }
+}
